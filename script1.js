608f3a674e43c2fae7fdf0801b8f4336c80ff092
NFaurass <36932043+NFaurass@users.noreply.github.com>
2020-01-15 00:22:39 +0000

    Delete script1.js

--- script1.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-  $("#SeasonSelector").change(function() {
-    $(".SeasonX").hide(), $("#" + $(this).val()).show()
-  })
-}), $(document).keydown(function(e) {
-  return 123 != e.keyCode && ((!e.ctrlKey || !e.shiftKey || 73 != e.keyCode) && void 0)
-});
